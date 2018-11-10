428adb204adbf41c941d9f52e3ef65a6fc216db6
Attila <daf276@gmx.de>
2018-11-10 15:57:23 +0100

    wrap image loading into a method

--- Data preprocessing/read_data.py
+++ Data preprocessing/read_data.py
@@ -3,27 +3,33 @@ import nibabel as nib
 import numpy as np
 import matplotlib.pyplot as plt
 
-path = "/home/kevinteng/Desktop/Medical Imaging/data/train/img" #directory to 'img' folder
-main_dir = sorted(os.listdir(path)) #file directory 
 
-img_list = []
-h = []
-s = []
+def load_images(path):
+    main_dir = sorted(os.listdir(path)) #file directory 
 
-for dir in main_dir:
-    data_path = os.path.join(path,dir)
-    img_load = nib.load(data_path)
-    n = img_load.get_data().astype(np.float32)
-    img_list.append(n) #image info stored in a list
-    h.append(img_load.shape[1]) #since weight and height are the same, only height is listed
-    s.append(img_load.shape[2]) #slices
+    img_list = []
+    h = []
+    s = []
 
-plt.hist(h, edgecolor = 'black', linewidth = 1.2)
-plt.xlabel('Width/Height')
-plt.ylabel('Amount')
-plt.show()
+    for dir in main_dir:
+        data_path = os.path.join(path,dir)
+        print(data_path)
+        img_load = nib.load(data_path)
+        n = img_load.get_data().astype(np.float32)
+        img_list.append(n) #image info stored in a list
+        h.append(img_load.shape[1]) #since width and height are the same, only height is listed
+        s.append(img_load.shape[2]) #slices
 
-plt.hist(s, color = 'orange', edgecolor = 'black', linewidth = 1.2)
-plt.xlabel('Slices')
-plt.ylabel('Amount')
-plt.show()
+    return img_list, h, s
+
+
+def show_histograms(h, s):
+    plt.hist(h, edgecolor = 'black', linewidth = 1.2)
+    plt.xlabel('Width/Height')
+    plt.ylabel('Amount')
+    plt.show()
+
+    plt.hist(s, color = 'orange', edgecolor = 'black', linewidth = 1.2)
+    plt.xlabel('Slices')
+    plt.ylabel('Amount')
+    plt.show()
---
+++ Data preprocessing/scale_and_pad.py
@@ -0,0 +1,3 @@
+from read_data import load_images
+
+images, height, depth, = load_images("../data/train/img")
