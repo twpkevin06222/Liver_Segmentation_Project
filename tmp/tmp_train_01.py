85581c0fbd34ccdd3ffcb37a3b6d6231c36b784c
Wai Po Kevin Teng <37227666+twpkevin06222@users.noreply.github.com>
2018-11-29 23:57:03 +0100

    Create tmp_train_01.py

---
+++ tmp/tmp_train_01.py
@@ -0,0 +1,190 @@
+import os
+import cv2
+from keras.layers import Input,Dense,Flatten,Dropout,merge,Reshape,Conv2D,MaxPooling2D,UpSampling2D,Conv2DTranspose
+from keras.layers.normalization import BatchNormalization
+from keras.models import Model,Sequential
+from keras.callbacks import ModelCheckpoint
+from keras.optimizers import Adadelta, RMSprop,SGD,Adam
+from keras import regularizers
+from keras import backend as K
+import numpy as np
+import scipy.misc
+import numpy.random as rng
+from PIL import Image, ImageDraw, ImageFont
+from sklearn.utils import shuffle
+import nibabel as nib #reading MR images
+from sklearn.model_selection import StratifiedKFold, cross_val_score
+import math
+import glob
+from matplotlib import pyplot as plt
+from read_data import load_images
+from keras.layers.merge import concatenate
+from sklearn.model_selection import train_test_split
+#img_list, height, depth, img_affine = load_images("/home/kevinteng/Desktop/Medical Imaging/img_new")
+
+def three_to_two(path, label='FALSE'):
+    ff = glob.glob(path)
+    images =[]
+    for f in range(len(ff)):
+        a = nib.load(ff[f])
+        a = a.get_data()
+        for i in range(a.shape[2]):
+            images.append(a[:,:,i])     
+    images = np.asarray(images)
+    images = images.reshape(-1, 128,128, 1) # dimension to feed into the network
+# =============================================================================
+#     if label=='TRUE':
+#         img_labels = np.place(images, images>1, 1) #tumor label not needed
+#         return img_labels
+#     else:
+#         return images
+# =============================================================================
+    return images
+
+def min_max_norm(images):
+    m = np.max(images)
+    mi = np.min(images)
+    images = (images - mi)/ (m - mi)
+    return images
+
+#Image for training 
+images = three_to_two(path='img_new/*')
+#images = min_max_norm(images)
+
+#labels
+img_labels = three_to_two(path='img_new_label/*')
+#img_labels = three_to_two(path='img_new_label/*', label='TRUE')
+
+#Parameters
+batch_size = 128
+epochs = 50
+inChannel = 1
+x, y = 128, 128
+input_img = Input(shape = (x, y, inChannel))
+
+#split data set
+train_x,valid_x,train_ground,valid_ground = train_test_split(images,
+                                                             img_labels,
+                                                             test_size=0.2,
+                                                             random_state=13)
+
+#Model
+def autoencoder(input_img):
+    #encoder
+    #input = 28 x 28 x 1 (wide and thin)
+    conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(input_img) #28 x 28 x 32
+    conv1 = BatchNormalization()(conv1)
+    conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv1)
+    conv1 = BatchNormalization()(conv1)
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1) #14 x 14 x 32
+    conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(pool1) #14 x 14 x 64
+    conv2 = BatchNormalization()(conv2)
+    conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv2)
+    conv2 = BatchNormalization()(conv2)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2) #7 x 7 x 64
+    conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(pool2) #7 x 7 x 128 (small and thick)
+    conv3 = BatchNormalization()(conv3)
+    conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv3)
+    conv3 = BatchNormalization()(conv3)
+    
+    #decoder
+    conv4 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv3) #7 x 7 x 128
+    conv4 = BatchNormalization()(conv4)
+    conv4 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv4)
+    conv4 = BatchNormalization()(conv4)
+    up1 = UpSampling2D((2,2))(conv4) # 14 x 14 x 128
+    conv5 = Conv2D(32, (3, 3), activation='relu', padding='same')(up1) # 14 x 14 x 64
+    conv5 = BatchNormalization()(conv5)
+    conv5 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv5)
+    conv5 = BatchNormalization()(conv5)
+    up2 = UpSampling2D((2,2))(conv5) # 28 x 28 x 64
+    decoded = Conv2D(1, (3, 3), activation='sigmoid', padding='same')(up2) # 28 x 28 x 1
+    return decoded
+
+def Unet(input_img, k_size=(3,3)):
+    merge_axis = -1 # Feature maps are concatenated along last axis (for tf backend)
+    data = input_img
+    conv1 = Conv2D(filters=32, kernel_size=k_size, padding='same', activation='relu')(data)
+    conv1 = Conv2D(filters=32, kernel_size=k_size, padding='same', activation='relu')(conv1)
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+
+    conv2 = Conv2D(filters=64, kernel_size=k_size, padding='same', activation='relu')(pool1)
+    conv2 = Conv2D(filters=64, kernel_size=k_size, padding='same', activation='relu')(conv2)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+
+    conv3 = Conv2D(filters=64, kernel_size=k_size, padding='same', activation='relu')(pool2)
+    conv3 = Conv2D(filters=64, kernel_size=k_size, padding='same', activation='relu')(conv3)
+    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+
+    conv4 = Conv2D(filters=128, kernel_size=k_size, padding='same', activation='relu')(pool3)
+    conv4 = Conv2D(filters=128, kernel_size=k_size, padding='same', activation='relu')(conv4)
+    pool4 = MaxPooling2D(pool_size=(2, 2))(conv4)
+
+    conv5 = Conv2D(filters=256, kernel_size=k_size, padding='same', activation='relu')(pool4)
+
+    up1 = UpSampling2D(size=(2, 2))(conv5)
+    conv6 = Conv2D(filters=256, kernel_size=k_size, padding='same', activation='relu')(up1)
+    conv6 = Conv2D(filters=256, kernel_size=k_size, padding='same', activation='relu')(conv6)
+    merged1 = concatenate([conv4, conv6], axis=merge_axis)
+    conv6 = Conv2D(filters=256, kernel_size=k_size, padding='same', activation='relu')(merged1)
+
+    up2 = UpSampling2D(size=(2, 2))(conv6)
+    conv7 = Conv2D(filters=256, kernel_size=k_size, padding='same', activation='relu')(up2)
+    conv7 = Conv2D(filters=256, kernel_size=k_size, padding='same', activation='relu')(conv7)
+    merged2 = concatenate([conv3, conv7], axis=merge_axis)
+    conv7 = Conv2D(filters=256, kernel_size=k_size, padding='same', activation='relu')(merged2)
+
+    up3 = UpSampling2D(size=(2, 2))(conv7)
+    conv8 = Conv2D(filters=128, kernel_size=k_size, padding='same', activation='relu')(up3)
+    conv8 = Conv2D(filters=128, kernel_size=k_size, padding='same', activation='relu')(conv8)
+    merged3 = concatenate([conv2, conv8], axis=merge_axis)
+    conv8 = Conv2D(filters=128, kernel_size=k_size, padding='same', activation='relu')(merged3)
+
+    up4 = UpSampling2D(size=(2, 2))(conv8)
+    conv9 = Conv2D(filters=64, kernel_size=k_size, padding='same', activation='relu')(up4)
+    conv9 = Conv2D(filters=64, kernel_size=k_size, padding='same', activation='relu')(conv9)
+    merged4 = concatenate([conv1, conv9], axis=merge_axis)
+    conv9 = Conv2D(filters=64, kernel_size=k_size, padding='same', activation='relu')(merged4)
+
+    conv10 = Conv2D(filters=1, kernel_size=k_size, padding='same', activation='sigmoid')(conv9)
+
+    output = conv10
+    model = Model(data, output)
+    return model
+
+def IoU(y_true, y_pred):
+    """Returns Intersection over Union score for ground truth and predicted masks."""
+    assert y_true.dtype == bool and y_pred.dtype == bool
+    y_true_f = y_true.flatten()
+    y_pred_f = y_pred.flatten()
+    intersection = np.logical_and(y_true_f, y_pred_f).sum()
+    union = np.logical_or(y_true_f, y_pred_f).sum()
+    return (intersection + 1) * 1. / (union + 1)
+
+def Dice(y_true, y_pred):
+    """Returns Dice Similarity Coefficient for ground truth and predicted masks."""
+    assert y_true.dtype == bool and y_pred.dtype == bool
+    y_true_f = y_true.flatten()
+    y_pred_f = y_pred.flatten()
+    intersection = np.logical_and(y_true_f, y_pred_f).sum()
+    return (2. * intersection + 1.) / (y_true.sum() + y_pred.sum() + 1.)
+
+# =============================================================================
+# autoencoder = Model(input_img, autoencoder(input_img))
+# autoencoder.compile(loss='categorical_crossentropy', optimizer = RMSprop())
+# autoencoder_train = autoencoder.fit(train_x, train_ground, batch_size=batch_size,epochs=epochs,verbose=1,validation_data=(valid_x, valid_ground))
+# =============================================================================
+unet = Unet(input_img)
+unet.compile(loss='mean_squared_error', optimizer = RMSprop(),metrics=['accuracy'])
+unet_train = unet.fit(train_x, train_ground, batch_size=batch_size,epochs=epochs,verbose=1,validation_data=(valid_x, valid_ground))
+# =============================================================================
+# loss = autoencoder_train.history['loss']
+# val_loss = autoencoder_train.history['val_loss']
+# epochs = range(3036)
+# plt.figure()
+# plt.plot(epochs, loss, 'bo', label='Training loss')
+# plt.plot(epochs, val_loss, 'b', label='Validation loss')
+# plt.title('Training and validation loss')
+# plt.legend()
+# plt.show()
+# =============================================================================
